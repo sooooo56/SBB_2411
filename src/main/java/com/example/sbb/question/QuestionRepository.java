5aa927d460d2f289fbafaf372caf0d56d71cff74
sooooo56 <sooooo4645@gmail.com>
2024-11-28 23:23:33 +0900

    페이징, 게시물 번호(수정), 답변갯수

--- src/main/java/com/example/sbb/question/QuestionRepository.java
+++ src/main/java/com/example/sbb/question/QuestionRepository.java
@@ -1,6 +1,10 @@
 package com.example.sbb.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
+
+    Page<Question> findAll(Pageable pageable);
 }
